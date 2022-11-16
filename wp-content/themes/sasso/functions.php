b30253c563554b610b9f66c61d1ba2aae7ba6381
SSCalifornia <45532321+SSCalifornia@users.noreply.github.com>
2022-11-15 22:54:33 -0800

    remove theme support, instantiate header/footer

--- wp-content/themes/sasso/functions.php
+++ wp-content/themes/sasso/functions.php
@@ -2,13 +2,16 @@
 
 // classes
 require_once( get_template_directory() . '/includes/classes/class-setup.php' );
-require_once( get_template_directory() . '/includes/classes/class-theme-support.php' );
 require_once( get_template_directory() . '/includes/classes/class-cleanup.php' );
 require_once( get_template_directory() . '/includes/classes/class-login.php' );
 require_once( get_template_directory() . '/includes/classes/class-menus.php' );
+require_once( get_template_directory() . '/includes/classes/class-theme-block.php' );
 
 new SASSO\Setup();
-new SASSO\Theme_Support();
 new SASSO\Cleanup();
 new SASSO\Login();
 new SASSO\Menus();
+
+// Theme Blocks
+new SASSO\Theme_Block( 'header' );
+new SASSO\Theme_Block( 'footer' );
