ced4bfe9c79de704fdac13f6490dec62341cf77b
SSCalifornia <45532321+SSCalifornia@users.noreply.github.com>
2024-02-28 01:35:39 -0800

    add helpers, new theme blocks

--- wp-content/themes/sasso/functions.php
+++ wp-content/themes/sasso/functions.php
@@ -6,6 +6,7 @@ require_once( get_template_directory() . '/includes/classes/class-cleanup.php' )
 require_once( get_template_directory() . '/includes/classes/class-login.php' );
 require_once( get_template_directory() . '/includes/classes/class-menus.php' );
 require_once( get_template_directory() . '/includes/classes/class-theme-block.php' );
+require_once( get_template_directory() . '/includes/helpers.php' );
 
 new SASSO\Setup();
 new SASSO\Cleanup();
@@ -15,3 +16,5 @@ new SASSO\Menus();
 // Theme Blocks
 new SASSO\Theme_Block( 'header' );
 new SASSO\Theme_Block( 'footer' );
+new SASSO\Theme_Block( 'page' );
+new SASSO\Theme_Block( 'single' );
