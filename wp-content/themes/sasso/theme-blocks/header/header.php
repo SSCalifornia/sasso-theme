ffa3ca6c7243b71bbe39c11d61ff1f8d5bb8bd2a
SSCalifornia <45532321+SSCalifornia@users.noreply.github.com>
2024-02-28 01:26:48 -0800

    rewrite header

--- wp-content/themes/sasso/theme-blocks/header/header.php
+++ wp-content/themes/sasso/theme-blocks/header/header.php
@@ -1,20 +1,20 @@
+<?php
+$header_menu = new SASSO\Menus;
+?>
+<a href="#site-content" class="site-header__skip-link">Skip to Content</a>
 <header class="site-header">
 	<div class="content-container">
-		<div class="site-header__nav">
-			<div class="site-header__nav-left">
-				<div class="site-header__nav-logo">
-					<a href="<?php echo esc_url( get_site_url( '/' ) ); ?>" title="<?php echo get_bloginfo( 'name' ); ?>"><?php echo get_bloginfo( 'name' ); ?></a>
-				</div>
+		<div class="site-header__grid">
+			<div class="site-header__logo">
+				<a href="<?php echo esc_url( get_site_url( '/' ) ); ?>" title="<?php echo get_bloginfo( 'name' ); ?>"><?php echo inline_svg( 'sasso-logo', '295.625', '164.063' ) ?></a>
 			</div>
-			<div class="site-header__nav-right">
-				<div class="site-header__nav-mobile-icon show-on-mobile"><?php echo esc_html__( 'Menu', 'sasso' ); ?></div>
-				<div class="site-header__nav-menu hide-on-mobile">
-					<?php
-					$header_nav = new SASSO\Menus;
-					$header_nav->sasso_header_nav();
-					?>
-				</div>
-			</div>
+			<button class="site-header__hamburger show-on-mobile"><span class="site-header__hamburger-patty"></span></button>
+			<nav class="site-header__navigation hide-on-mobile">
+				<?php $header_menu->sasso_header_nav(); ?>
+			</nav>
 		</div>
 	</div>
 </header>
+<?php
+$header_menu->sasso_mobile_menu( 1 );
+?>
