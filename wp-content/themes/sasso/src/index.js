462dcbe1aa1de07df0ff9e0ff2a523fbae286680
SSCalifornia <45532321+SSCalifornia@users.noreply.github.com>
2024-02-28 01:28:14 -0800

    remove slide menu, add mobile menu/variables js

--- wp-content/themes/sasso/src/index.js
+++ wp-content/themes/sasso/src/index.js
@@ -2,4 +2,11 @@
 import './assets/scss/style.scss'
 
 // js
-import './assets/scripts/js/mobile-menu.js'
+//import './assets/scripts/sliding-mobile-menu.js'
+import Variables from './assets/scripts/variables.js';
+import SiteMobileMenu from './assets/scripts/site-mobile-menu.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+	new Variables();
+	new SiteMobileMenu();
+});
