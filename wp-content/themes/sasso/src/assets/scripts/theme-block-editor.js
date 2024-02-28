41ee33b94c405022ddc381816e6949deef616749
SSCalifornia <45532321+SSCalifornia@users.noreply.github.com>
2024-02-28 01:33:17 -0800

    move to scripts folder

---
+++ wp-content/themes/sasso/src/assets/scripts/theme-block-editor.js
@@ -0,0 +1,7 @@
+wp.domReady( () => {
+
+	// unregister core block styles
+	wp.blocks.unregisterBlockStyle( 'core/button', 'fill' );
+	wp.blocks.unregisterBlockStyle( 'core/button', 'outline' );
+	wp.blocks.unregisterBlockStyle( 'core/image', 'rounded' );	
+});
