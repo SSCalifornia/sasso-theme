e8fb07f31410c06e85f824102986d0c0154a298e
SSCalifornia <45532321+SSCalifornia@users.noreply.github.com>
2024-02-28 01:26:56 -0800

    rewrite footer

--- wp-content/themes/sasso/theme-blocks/footer/footer.php
+++ wp-content/themes/sasso/theme-blocks/footer/footer.php
@@ -1,8 +1,11 @@
 <footer class="site-footer">
-	<div class="content-container"></div>
+	<div class="content-container">
+		<div class="site-footer__copyright">&copy; <?php echo date( 'Y' ); ?> <?php echo __( 'Sasso', 'sasso' ); ?></div>
+	</div>
 </footer> <!-- /site-footer -->
 
 <?php
-$mobile_nav = new SASSO\Menus;
-$mobile_nav->sasso_mobile_menu( 3 );
+// sliding mobile menu
+//$mobile_nav = new SASSO\Menus;
+//$mobile_nav->sasso_mobile_menu( 3, 'sliding', true );
 ?>
